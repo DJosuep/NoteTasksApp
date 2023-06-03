45c0d6a14e813baf3248807cdb2172005a4edd8b
DJosuep <josuep.garciar@gmail.com>
2023-06-03 11:24:26 -0600

    Añadiendo proyecto y pantalla principal
    - Añadiendo package screens.main y preview en MainScreen.

---
+++ app/src/main/java/com/djosuep/notetasks/ui/screens/main/MainScreen.kt
@@ -0,0 +1,15 @@
+package com.djosuep.notetasks.ui.screens.main
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun MainScreen(){
+
+}
+
+@Composable
+@Preview
+fun PreviewMainScreen(){
+    MainScreen()
+}
