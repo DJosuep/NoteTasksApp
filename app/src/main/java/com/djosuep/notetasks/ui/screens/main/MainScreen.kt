fbb66a44df1cf8a2a2ed1a31e22eea493ac7371f
DJosuep <josuep.garciar@gmail.com>
2023-06-03 11:42:00 -0600

    Experimental Material API and theme
    - Added theme.
    - Updated gradles.
    - Added material gradle.
    - Added BackdropScaffold into the MainsScreen.

--- app/src/main/java/com/djosuep/notetasks/ui/screens/main/MainScreen.kt
+++ app/src/main/java/com/djosuep/notetasks/ui/screens/main/MainScreen.kt
@@ -1,13 +1,29 @@
 package com.djosuep.notetasks.ui.screens.main
 
+import androidx.compose.material.BackdropScaffold
+import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 
+@ExperimentalMaterialApi
 @Composable
-fun MainScreen(){
+fun MainScreen() {
+    BackdropScaffold(
+        appBar = {
 
+        },
+        frontLayerContent = {
+
+        },
+        backLayerContent = {
+
+        }
+    ) {
+
+    }
 }
 
+@ExperimentalMaterialApi
 @Composable
 @Preview
 fun PreviewMainScreen(){
